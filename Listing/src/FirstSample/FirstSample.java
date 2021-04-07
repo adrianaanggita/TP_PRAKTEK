d8e6ad5a5a1fd0fe2da39090d01371ec4b1f52b4
adrianaanggita <anggitadaeli@gmail.com>
2021-04-07 16:57:42 +0700

    listing

---
+++ Listing/src/FirstSample/FirstSample.java
@@ -0,0 +1,16 @@
+package FirstSample;
+
+/**
+ * This is teh first sample program in Core Java Chapter 3
+ * @version apa aja
+ * @author Gary Cornell
+ */
+
+    public class FirstSample {
+
+        public static void main (String[] args) {
+
+            System.out.println("We will not use 'Hello, World!'");
+        }
+    }
+
