fc678a47b8a92b99766da8bdb86646f8aabc0759
adrianaanggita <anggitadaeli@gmail.com>
2021-04-18 08:35:59 +0700

    Assignment week 5

---
+++ Kasus3NarutoAnime/src/Sasuke.java
@@ -0,0 +1,10 @@
+import static java.lang.System.*;
+
+public class Sasuke extends Itachi {
+    String Dojutsu = "Sharingan";
+
+    void printDojutsu() {
+        super.printDojutsu();
+        out.println(this.Dojutsu);
+    }
+}
